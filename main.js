211caf589ca7f6dc9ae6d9a9099e4b71cd5f7049
jman9374 <jastaire96@gmail.com>
2021-06-22 15:46:33 -0500

    first edit

---
+++ main.js
@@ -0,0 +1,27 @@
+let today = new Date().getDay()
+let sunday = 0
+let monday = 1
+let tuesday = 2
+let wednesday = 3
+let thursday = 4
+let friday = 5
+let saturday = 6
+
+let hoursLogged = 34
+let username = prompt("Enter your username here:")
+let fridayIsHoliday = false
+
+let greeting 
+if (today === monday) {
+
+} else if (today === tuesday) {
+
+} else if (today === wednesday) {
+
+} else if (today === thursday) {
+
+} else if (today === friday) {
+
+} else {
+
+}
